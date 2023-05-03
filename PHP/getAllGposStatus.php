d9613515ca15174fbdd12c8f16f05df8ace4a747
Aestralylia <44844560+Aestralylia@users.noreply.github.com>
2023-05-03 15:47:51 +0200

    mise à jour du script powershell pour récupérer le statut des gpo + première version de traitement

--- PHP/getAllGposStatus.php
+++ PHP/getAllGposStatus.php
@@ -5,16 +5,81 @@
  * Objectif : éxécuter le script powershell préparé afin de récupérer le nom et le statut de toutes les gpos existantes,
  * puis de les écrire dans un fichier cvs   
  * Date de début : 21/04/202
- * Dernière modification : 21/04/2023 
+ * Dernière modification : 03/05/2023 
  ****************************************************
  */
 
 /**
  * Execution du script powershell récupérant le nom et le status de toutes les gpos sous forme de tableau : {[gpoName, status],[gpoName, status]}
  */
-$pathScriptGetAllGposStatus = '..\RessourcesFiles\GetAllGposStatus.ps1';
+$pathScriptGetAllGposStatus = 'RessourcesFiles\GetAllGposStatus.ps1';
 $prepare = 'powershell -executionpolicy remotesigned -command "& {"'.$pathScriptGetAllGposStatus.'"; exit $err}"';
-$result = shell_exec($prepare);
+$resultPowershell = shell_exec($prepare);
+
+$resultPowershell = "Verrouiller_cmd
+AllSettingsDisabled
+\\0
+Verrouiller_compte
+AllSettingsDisabled
+\\0
+Verrouiller_registre
+AllSettingsDisabled
+\\0
+Verrouiller_usb
+AllSettingsDisabled
+\\0";
+echo("Chaine initiale : " . $resultPowershell . "<br>");
+
+/**
+ * Initialisation des variables nécessaires au traitelement de la chaine retournée par le script powershell
+ */
+$currentStr = $resultPowershell;
+$chaineDeControle = "\\0";
+$resultTraitement = array();
+
+/**
+ * Boucle de traitement du résultat du script powershell
+ */
+while(true) {
+    echo("------------------------------". "<br>");
+    echo("currentStr : ". $currentStr . "<br>");
+
+    /**
+     * Retourne l'offset de la première occurence de la chaine \0.
+     * Pour rappel, la chaine de caractère possède le format suivant : nomGPO statusGPO \0 nomGPO statusGPO \0 ...
+     */
+    $offset = strpos($currentStr, $chaineDeControle);
+    echo("offset : " . $offset) . "<br>";
+
+    /**
+     * Si l'occurrence \0 n'a pas été trouvé, alors strpos retourne null.
+     * Dans ce cas, on stop la boucle de traitement while.
+     * Sinon, on effectue le traitement
+     */
+    if(  !$offset ) {
+        break 1;
+    } else {
+
+        /**
+         * On ajoute la chaine de caractère correspondante à la GPO actuelle dans le tableau de résultat du traitement
+         */
+        echo( stristr($currentStr, $chaineDeControle, true) . "<br>" );
+        array_push($resultTraitement, stristr($currentStr, $chaineDeControle, true));
+
+        /**
+         * Retourne la chaine de caractère depuis la première occurence de la chaine \0.
+         * La chaine \0 fait 2 caractères. Afin de ne pas prendre en compte le caractère espace, la sous-chaine part de offset+3.
+         * Phase de test : caractère retour chariot : +3 ==> +4
+         */
+        $currentStr = substr($currentStr, $offset+4);
+        echo("currentStr :". $currentStr . "<br>");
+    }
+}
+
+/**
+ * Affichage du tableau resultTraitement
+ */
+var_dump($resultTraitement);
 
 /**
  * Ecriture du status de toutes les gpos dans le fichier RessourcesFiles\AllGposStatus.csv
--- RessourcesFiles/GetAllGposStatus.ps1
+++ RessourcesFiles/GetAllGposStatus.ps1
@@ -1,1 +1,25 @@
-﻿return "Hello World !"
+﻿# return "Hello World !"
+
+$name_verrouiller_cmd = (Get-GPO Verrouiller_cmd).DisplayName
+$status_verrouiller_cmd = (Get-GPO Verrouiller_cmd).GpoStatus
+$fin_verrouiller_cmd = "\0";
+$gpo_verrouiller_cmd = @($name_verrouiller_cmd, $status_verrouiller_cmd, $fin_verrouiller_cmd)
+
+$name_verrouiller_compte = (Get-GPO Verrouiller_compte).DisplayName
+$status_verrouiller_compte = (Get-GPO Verrouiller_compte).GpoStatus
+$fin_verrouiller_compte =  "\0";
+$gpo_verrouiller_compte = @($name_verrouiller_compte, $status_verrouiller_compte, $fin_verrouiller_compte)
+
+$name_verrouiller_registre = (Get-GPO Verrouiller_registre).DisplayName
+$status_verrouiller_registre = (Get-GPO Verrouiller_registre).GpoStatus
+$fin_verrouiller_registre =  "\0";
+$gpo_verrouiller_registre = @($name_verrouiller_registre, $status_verrouiller_registre, $fin_verrouiller_registre)
+
+$name_verrouiller_usb = (Get-GPO Verrouiller_usb).DisplayName
+$status_verrouiller_usb = (Get-GPO Verrouiller_usb).GpoStatus
+$fin_verrouiller_usb =  "\0";
+$gpo_verrouiller_usb = @($name_verrouiller_usb, $status_verrouiller_usb, $fin_verrouiller_usb)
+
+$all_gpo = @($gpo_verrouiller_cmd,$gpo_verrouiller_compte,$gpo_verrouiller_registre,$gpo_verrouiller_usb)
+
+return $all_gpo;
